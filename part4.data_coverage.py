e55f2f0e835eab2753fcd2f83a5cf6790fffa9a1
Claire Richardson <crricha5@asu.edu>
2024-05-27 15:14:52 -0700

    part4 update

--- part4.data_coverage.py
+++ part4.data_coverage.py
@@ -1,30 +1,28 @@
+import glob
+import time
+import mod_input
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import mod_input
-import glob
 
-sector_nos = []
-min_extents = []
-max_extents = []
-sector_val = 0.
+df_shells = pd.read_csv(mod_input.shell_file)
+df_blocks = pd.read_csv(mod_input.block_file)
+df_all = pd.DataFrame()
+all_shells = df_shells['SHELL#']
+all_blocks = df_blocks['BLOCK#']
+total_phases = len(mod_input.all_phases)
+azimuthal_sector_extent = 180. / mod_input.azimuthal_sectors
 
-header_list = ['SHELL#', 'BLOCK#', 'TOTAL_PATHS', 'TOTAL_SECTORS']
-for i in range(mod_input.azimuthal_sectors):
-    sector_no = int(i + 1)
-    sector_nos.append(sector_no)
-    min_extents.append(sector_val)
-    sector_val += mod_input.azimuthal_sector_extent
-    max_extents.append(sector_val)
-    header_name = f'SECTOR_{sector_no}'
-    header_list.append(header_name)
-    df_sectors = pd.DataFrame(data = {'sector': sector_nos, 'min_extent': min_extents, 'max_extent': max_extents})    
-header_str = ','.join(header_list)
+df_sectors = pd.DataFrame(columns = ['sector', 'min_extent', 'max_extent'])
+df_sectors['sector'] = list(range(1, mod_input.azimuthal_sectors + 1))
+df_sectors['min_extent'] = (df_sectors['sector'] - 1) * azimuthal_sector_extent
+df_sectors['max_extent'] = df_sectors['sector'] * azimuthal_sector_extent
 
 def azimuthal_sector(az):
     '''
     define a function to determin the azimuthal sector of an input azimuth
     '''
+    if az == 360.:
+        az = 0.
     if az >= 180.:
         az -= 180.
     for line in range(len(df_sectors)):
@@ -33,13 +31,6 @@ def azimuthal_sector(az):
         x = df_sectors['max_extent'].iloc[line]
         if az >= n and az < x:
             return int(s)
-
-df_shells = pd.read_csv(mod_input.shell_file)
-df_blocks = pd.read_csv(mod_input.block_file)
-df_all = pd.DataFrame()
-all_shells = df_shells['SHELL#']
-all_blocks = df_blocks['BLOCK#']
-total_phases = len(mod_input.all_phases)
 
 # make the master dataframe for all of the sectors defined in mod_input. this will be the empty dataframe for ALL phase information
 sh_col_all = []
@@ -52,15 +43,17 @@ df_all['SHELL#'] = sh_col_all
 df_all['BLOCK#'] = bl_col_all
 df_all['TOTAL_PATHS'] = 0
 df_all['TOTAL_SECTORS'] = 0
-for az_sector in sector_nos:
+for az_sector in df_sectors['sector']:
     df_all[f'SECTOR_{az_sector}'] = 0
 
-phases_completed = []
-# find azimuthal coverage for all phases, by phase.
-for phase in mod_input.all_phases:
-    phases_completed.append(phase)
-    print(f'working on {phase} coverage')
-    paths = glob.glob(f'./{mod_input.phases_directory}/{phase}{mod_input.dataset_name_mod}/{mod_input.resampled_paths_directory}/{phase}_*.csv')
+
+def find_phase_coverage(phase):
+    try:
+        os.remove(f'./{phases_directory}/{phase}/{data_directory}/{phase}_pt4_coverage_log.txt')
+    except:
+        pass
+    start_coverage_time = time.time()
+    paths = glob.glob(f'./{mod_input.phases_directory}/{phase}/{mod_input.paths_directory}/{phase}_*.csv')
     df_phase = pd.DataFrame()
     
     # make the master dataframe for all of the sectors defined in mod_input. this will be the empty dataframe for each individual phase.
@@ -79,15 +72,13 @@ for phase in mod_input.all_phases:
     
     total_paths = len(paths)
     itr = 0
-    percent_completes = []    
-
+    percent_completes = []
+    
     # for each path file, find all of the unique block & shell id combinations that the ray passes through    
     for path in paths:
         itr += 1
-        percent_complete = int((itr / total_paths) * 100)
-        if percent_complete not in percent_completes:
-            percent_completes.append(percent_complete)
-            print(f'-  computing phase specific coverage; (phase {len(phases_completed)} of {total_phases}): {percent_complete}% complete')
+        with open(f'./{phases_directory}/{phase}/{data_directory}/{phase}_pt4_coverage_log.txt', 'a') as fout:
+            fout.write(f'- working on path {itr} of {len(paths)}\n')
         df_p = pd.read_csv(path)
         df_slice = df_p[['SEG_SHELL#', 'SEG_BLOCK#']]
         df_count = df_slice.groupby(['SHELL#','BLOCK#']).size().reset_index().rename(columns = {0: 'TOTAL_PATHS'})
@@ -113,13 +104,36 @@ for phase in mod_input.all_phases:
             # add one to the count of the specific segment azimuthal sector for the current combo in the phase specific dataframe
             df_phase.loc[element_idx, f'SECTOR_{sector}'] += 1
 
-    np.savetxt(f'./{mod_input.phases_directory}/{phase}{mod_input.dataset_name_mod}/{mod_input.data_directory}/coverage_{phase}.csv', df_phase, fmt = '%i', delimiter = ',', header = header_str, comments = '')
+    df_phase.to_csv(f'./{mod_input.phases_directory}/{phase}/{mod_input.data_directory}/{phase}_coverage.csv', index = False)
+    with open(f'./{phases_directory}/{phase}/{data_directory}/{phase}_pt4_coverage_log.txt', 'a') as fout:
+        fout.write(f'FINISHED; total time: {(time.time() - start_coverage_time) / 60 minutes; {((time.time() - start_coverage_time) / 60) / 60} hours\n')
+
+
+## Start parallel processes:
+print(f'START PARSING COVERAGE')
+coverage_start = time.time()
+
+if __name__ == '__main__':
+    process_list = []
+    process_idx = 0
+    for phase in mod_input.all_phases:
+        process_idx += 1
+        print(f'  - starting process {process_idx} of {len(mod_input.all_phases)}')
+
+        p = mp.Process(target = find_phase_coverage, args = (phase,))
+        p.start()
+        process_list.append(p)
+
+    for process in process_list:
+        process.join()
+
+coverage_time = time.time() - coverage_start
+print(f'FINISHED PARSING COVERAGE; runtime: {coverage_time / 60} minutes / {(coverage_time / 60) / 60} hours')
 
 
 # loop through each newly made individual coverage file to compile the sums into the master/total coverage file.
 for phase in mod_input.all_phases:
-    print(f'compiling files, phase: {phase}')
-    df_phase_coverage = pd.read_csv(f'./{mod_input.phases_directory}/{phase}{mod_input.dataset_name_mod}/{mod_input.data_directory}/coverage_{phase}.csv')
+    df_phase_coverage = pd.read_csv(f'./{mod_input.phases_directory}/{phase}/{mod_input.data_directory}/{phase}_coverage.csv')
     df_all['TOTAL_PATHS'] = df_all['TOTAL_PATHS'] + df_phase_coverage['TOTAL_PATHS']
 
     for sector in sector_nos:
@@ -134,7 +148,6 @@ for line in range(len(df_all)):
     total_element_idx = df_all.loc[(df_all['SHELL#'] == total_shell_no) & (df_all['BLOCK#'] == total_block_no)].index
     sector_count = 0
     if total_shell_no not in shells:
-        print(f'working on shell {total_shell_no}')
         shells.append(total_shell_no)
     for sector in sector_nos:
         if int(df_all.loc[total_element_idx, f'SECTOR_{sector}']) != 0:
@@ -142,5 +155,9 @@ for line in range(len(df_all)):
 
     df_all.loc[total_element_idx, 'TOTAL_SECTORS'] = sector_count
 
-np.savetxt(f'coverage_all{mod_input.dataset_name_mod}.csv', df_all, fmt = '%i', delimiter = ',', header = header_str, comments = '')
+df_all.to_csv(f'./{mod_input.phases_directory}/total_coverage.csv', index = False)
+
+
+
+
 
